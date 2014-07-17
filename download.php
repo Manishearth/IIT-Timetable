23055d8187556f66e2c87fa0a4e86aec7f4cfe7c
cheekujodhpur <cheekujodhpur@gmail.com>
2014-07-17 09:21:53 +0530

    Revert "Export as .ics"
    This reverts commit 21fa7fbc7ccb296628cb5e7d930a8e210b876efe.

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-disposition: attachment; filename=calendar.ics');
-header('Content-type: text/plain');
-$text = $_POST['codeICS'];
-$breaks = array("<br />","<br>","<br/>");  
-$code = str_ireplace($breaks, "\r\n", $text); 
-
-echo $code;
-?>
